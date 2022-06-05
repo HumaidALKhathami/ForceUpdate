966c841c6e0efc2d247da3a305a9fc9ce29649d6
humaidalkhathami <humaid.ali.jed.k2@tuwaiq.edu.sa>
2022-06-05 13:57:09 +0300

    solution #8

--- forceupdate/src/main/java/com/android/forceupdate/broadcast/InstallBroadcastReceiver.kt
+++ forceupdate/src/main/java/com/android/forceupdate/broadcast/InstallBroadcastReceiver.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.EXTRA_INTENT
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.pm.PackageInfo
 import android.content.pm.PackageInstaller
+import android.content.pm.PackageManager
 import com.android.forceupdate.repository.install.InstallRepositoryImpl.InstallStatus
 import com.android.forceupdate.repository.install.InstallRepositoryImpl.InstallStatus.*
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,13 +15,23 @@ import kotlinx.coroutines.flow.asStateFlow
 
 internal class InstallBroadcastReceiver : BroadcastReceiver() {
 
+
     override fun onReceive(context: Context, intent: Intent) {
 
         when (intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)) {
             PackageInstaller.STATUS_PENDING_USER_ACTION -> {
-                val installIntent = intent.getParcelableExtra<Intent>(EXTRA_INTENT)
-                context.startActivity(installIntent?.addFlags(FLAG_ACTIVITY_NEW_TASK))
-                mutableInstallBroadcastState.value = InstallProgress
+
+                when (PackageManager.GET_PERMISSIONS){
+                    PackageManager.PERMISSION_GRANTED -> {
+                        val installIntent = intent.getParcelableExtra<Intent>(EXTRA_INTENT)
+                        context.startActivity(installIntent?.addFlags(FLAG_ACTIVITY_NEW_TASK))
+                        mutableInstallBroadcastState.value = InstallProgress
+                    }
+                    PackageManager.PERMISSION_DENIED-> {
+                        mutableInstallBroadcastState.value = InstallCanceled
+                    }
+
+                }
             }
             PackageInstaller.STATUS_SUCCESS -> {
                 mutableInstallBroadcastState.value = InstallSucceeded
